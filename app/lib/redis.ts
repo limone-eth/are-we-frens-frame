c56c3eb7edd8c94fa90088ee9ae598e45ef0240a
lim0n3 <simonestaffa96@gmail.com>
2024-01-30 23:40:09 +0100

    console log

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -18,5 +18,6 @@ export const setClaimed = async (address: string) => {
 
 export const hasClaimed = async (address: string) => {
   const claimed = await kv.get(address);
+  console.log(claimed);
   return claimed && claimed === 'claimed';
 };
